db57c3931a9fb238b7c5be2c7826dd7ab6f27a91
ellisredding2016 <aaron.rowan@gmail.com>
2016-06-22 14:28:04 -0400

    Updating Published Docs
    Updating the Published Branch with a version that has a error handler of sorts.

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ var guessCount=0;
 
 
 $(document).ready(function(){
+	
 	keyValue = (Math.floor((Math.random()*100 +1)));
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -31,42 +32,70 @@ $(document).ready(function(){
 
   	/*--- Guess Routine ---*/
   	$("#guessButton").click(function(event){
+  		var inputError = 0;
+  		var error
   		event.preventDefault(); /*--- keep guessButton of type-> submit from reloading  ---*/
-  		guessCount++;
-  		guessValue = $("#userGuess").val();
-  		
-  		/*--- calculate distance from target ---*/
-  		var error = Math.abs(guessValue - keyValue);
-  		var guessClass;
-  		
-  		$("#count").text(guessCount);  //update count of guesses.
+		
+		inputError = checkInt($("#userGuess").val());
+  		  		
+  		if(inputError===0){
+	  		guessCount++;
+	  		guessValue = $("#userGuess").val();
+	  		
+	  		/*--- calculate distance from target ---*/
+	  		var error = Math.abs(guessValue - keyValue);
+	  		var guessClass;
+	  		
+	  		$("#count").text(guessCount);  //update count of guesses.
+	  		
+	  		/*--- Customize Feedback Based on Distance from Target ---*/
+	  		if(error ==0){
+	  			$('#feedback').text("You Got It!");
+	  			guessClass="gotIt"
+	  		}
+	  		else if(error < 3){
+	  			$('#feedback').text("You are REALLY Warm");
+	  			guessClass="hot"
+	  		}
+	  		else if(error < 10){
+	  			$('#feedback').text("You Are Very Warm");
+	  			guessClass="warm"
+	  		}
+	  		else if(error < 25){
+	  			$('#feedback').text("You are Warm");
+	  			guessClass="tepid"
+	  		}
+	  		else {
+	  			$('#feedback').text("You are Cold");
+	  			guessClass="cold"
+	  		}
 
-  		/*--- Customize Feedback Based on Distance from Target ---*/
-  		if(error ==0){
-  			$('#feedback').text("You Got It!");
-  			guessClass="gotIt"
-  		}
-  		else if(error < 3){
-  			$('#feedback').text("You are REALLY Warm");
-  			guessClass="hot"
-  		}
-  		else if(error < 10){
-  			$('#feedback').text("You Are Very Warm");
-  			guessClass="warm"
-  		}
-  		else if(error < 25){
-  			$('#feedback').text("You are Warm");
-  			guessClass="tepid"
-  		}
-  		else {
-  			$('#feedback').text("You are Cold");
-  			guessClass="cold"
-  		}
-
-  		/*--- Add to List of Guesses  ---*/
-  		$("#guessList").append("<li class="+ guessClass + ">"+guessValue+"</li>");
-  		$("#userGuess").val("");
+	  		/*--- Add to List of Guesses  ---*/
+	  		$("#guessList").append("<li class="+ guessClass + ">"+ guessValue +"</li>");
+	  		$("#userGuess").val("");
+	  	}
   	})
 });
 
 
+function checkInt(input) {
+  	var error = 0;
+  	
+  	input = Math.ceil(input);
+  	//    console.log(input);
+  	input = parseInt(input);
+  	//console.log(input);
+  	
+  	if(isNaN(input)){ 
+    	//console.log("NaN");
+	    alert("Not a Valid Integer"); 
+	    error=1;
+  	}
+  	if(input > 100 || input < 0){ 
+  		alert("Enter a Number Between 0 and 100");
+		error=1;
+  	}
+
+  	return error;
+	
+}
